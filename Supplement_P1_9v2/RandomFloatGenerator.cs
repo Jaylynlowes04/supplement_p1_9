b37129d93a7b0b7a53243cb2456b7b62d4ef8616
Jaylyn Lowes <jlow7077@students.vsu.edu>
2025-02-27 23:12:17 -0500

    chore: added method for RandomFloatGenerator

--- Supplement_P1_9v2/RandomFloatGenerator.cs
+++ Supplement_P1_9v2/RandomFloatGenerator.cs
@@ -1,4 +1,12 @@
-public class RandomFloatGenerator
+using System.Collections;
+
+public class RandomFloatGenerator : IEnumerable<float>
 {
-    
+    private Random _random = new Random();
+    public IEnumerator<float> GetEnumerator()
+    {
+        throw new NotImplementedException();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
 }
