a62d037450ff31d7d2f59d1217f792d3e02b6352
Jaylyn Lowes <jlow7077@students.vsu.edu>
2025-02-27 23:27:51 -0500

    feat: implemented Quarter

--- Supplement_P1_9v2/Quarter.cs
+++ Supplement_P1_9v2/Quarter.cs
@@ -5,6 +5,19 @@ public class Quarter
     private float value;
     public Quarter(float value)
     {
-        throw new NotImplementedException();
+        if (value < 0.0f || value >= 1.0f)
+            throw new ArgumentOutOfRangeException("Value must be in range [0.0, 1.0)");
+        this.value = value;
     }
+    private int GetQuarter() => (int)(value * 4);
+
+    public static bool operator ==(Quarter q1, Quarter q2) => q1.GetQuarter() == q2.GetQuarter();
+    public static bool operator !=(Quarter q1, Quarter q2) => !(q1 == q2);
+    public static bool operator >(Quarter q1, Quarter q2) => q1.value > q2.value;
+    public static bool operator <(Quarter q1, Quarter q2) => q1.value < q2.value;
+    public static bool operator >=(Quarter q1, Quarter q2) => q1.value >= q2.value;
+    public static bool operator <=(Quarter q1, Quarter q2) => q1.value <= q2.value;
+    
+    public override bool Equals(object obj) => obj is Quarter q && this == q;
+    public override int GetHashCode() => GetQuarter().GetHashCode();
 }
