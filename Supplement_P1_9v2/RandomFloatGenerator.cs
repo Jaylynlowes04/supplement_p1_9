71dbf104b0c6698a13cb556806e54ebd9947733a
Jaylyn Lowes <jlow7077@students.vsu.edu>
2025-02-27 23:29:23 -0500

    feat: implemented RandomFloatGenerator

--- Supplement_P1_9v2/RandomFloatGenerator.cs
+++ Supplement_P1_9v2/RandomFloatGenerator.cs
@@ -5,7 +5,19 @@ public class RandomFloatGenerator : IEnumerable<float>
     private Random _random = new Random();
     public IEnumerator<float> GetEnumerator()
     {
-        throw new NotImplementedException();
+        int count = 0;
+        while (true)
+        {
+            float value = (float)_random.NextDouble();
+            yield return value;
+
+            count = value <= 0.5f ? count + 1 : 0;
+
+            if (count >= 3)
+            {
+                throw new InvalidSequenceException("Three consecutive numbers were ≤ 0.5");
+            }
+        }
     }
 
     IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
