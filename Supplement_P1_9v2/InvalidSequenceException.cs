2ccb2deadc72f1ec78de1e0bc3b9b24157a77327
Jaylyn Lowes <jlow7077@students.vsu.edu>
2025-02-27 23:55:49 -0500

    doc: Added document for InvalidSequenceException

--- Supplement_P1_9v2/InvalidSequenceException.cs
+++ Supplement_P1_9v2/InvalidSequenceException.cs
@@ -1,3 +1,6 @@
+/// <summary>
+/// Exception class that is thrown when an invalid sequence is generated.
+/// </summary>
 public class InvalidSequenceException : Exception
 {
     public InvalidSequenceException(string message) : base(message) { }
